aa17253fbe94cace89d98ee1bf4b5678b1f9d7c6
victorsmits <victor.smits@student.vinci.be>
2019-02-28 14:30:00 +0100

    Cabinet + Cart code start

--- Code Source/KitBoxSourceCode/KitBoxSourceCode/Cabinet.cs
+++ Code Source/KitBoxSourceCode/KitBoxSourceCode/Cabinet.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 namespace KitBoxSourceCode
 {
     public class Cabinet
     {
+        private List<StorageBox> StorageBoxes;
+
         public Cabinet()
         {
+        }
+
+        public void AddStorageBox(StorageBox storage)
+        {
+            StorageBoxes.Add(storage);
         }
     }
 }
