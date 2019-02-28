82a623e1235c194823bc5d39467e8aa047e6a15c
victorsmits <victor.smits@student.vinci.be>
2019-02-28 18:37:41 +0100

    box readonly parameter

--- Code Source/KitBoxSourceCode/KitBoxSourceCode/Box.cs
+++ Code Source/KitBoxSourceCode/KitBoxSourceCode/Box.cs
@@ -2,91 +2,87 @@
 using System.Collections.Generic;
 namespace KitBoxSourceCode
 {
-    public class Box : StorageBox
-    {
-        private List<Compoment> Compoments;
-        private int Price = 0;
-        private int Lenght;
-        private int Height;
-        private int Depth;
+	public class Box : StorageBox
+	{
+		private List<Compoment> Compoments;
+		private int Price = 0;
+		private readonly int Lenght;
+		private readonly int Height;
+		private readonly int Depth;
 
-        public Box(int len, int height, int depth)
-        {
-            Compoments = new List<Compoment>();
-            Lenght = len;
-            Height = height;
-            Depth = depth;
+		public Box(int len, int height, int depth)
+		{
+			Compoments = new List<Compoment>();
+			Lenght = len;
+			Height = height;
+			Depth = depth;
 
-            AddPanel(len, height, depth);
-            AddCleat(height);
-            AddBeam(len, depth);
-            AddDoorBeam(len);
-        }
+			AddPanel(len, height, depth);
+			AddCleat(height);
+			AddBeam(len, depth);
+			AddDoorBeam(len);
+		}
 
-        public int GetPrice()
-        {
-            Compoments.ForEach(AddPrice);
-            return Price;
-        }
+		public int GetPrice()
+		{
+			Compoments.ForEach(AddPrice);
+			return Price;
+		}
 
-        public List<Compoment> GetCompoments => Compoments;
+		public List<Compoment> GetCompoments => Compoments;
 
-        public void GetDetails()
-        {
+		public void GetDetails()
+		{
 
-        }
+		}
 
-        private void AddPrice(Compoment elem)
-        {
-            Price += elem.GetPrice();
-        }
+		private void AddPrice(Compoment elem)
+		{
+			Price += elem.GetPrice();
+		}
 
-        private void AddPanel(int x, int y, int z)
-        {
-            Compoments.Add(new Panel(x, y));
+		private void AddPanel(int x, int y, int z)
+		{
+			Compoments.Add(new Panel(x, y));
 
-            Compoments.Add(new Panel(z, y));
-            Compoments.Add(new Panel(z, y));
+			Compoments.Add(new Panel(z, y));
+			Compoments.Add(new Panel(z, y));
 
-            Compoments.Add(new Panel(z, x));
-            Compoments.Add(new Panel(z, x));
-        }
+			Compoments.Add(new Panel(z, x));
+			Compoments.Add(new Panel(z, x));
+		}
 
-        private void AddCleat(int height)
-        {
-            int i = 0;
-            while (i < 4)
-            {
-                Compoments.Add(new Cleat(height));
-                i++;
-            }
-        }
+		private void AddCleat(int height)
+		{
+			int i = 0;
+			while (i < 4) {
+				Compoments.Add(new Cleat(height));
+				i++;
+			}
+		}
 
-        private void AddBeam(int len, int height)
-        {
-            int i = 0;
-            while (i < 4)
-            {
-                Compoments.Add(new Beam(len));
-                i++;
-            }
+		private void AddBeam(int len, int height)
+		{
+			int i = 0;
+			while (i < 4) {
+				Compoments.Add(new Beam(len));
+				i++;
+			}
 
-            int j = 0;
-            while (j < 2)
-            {
-                Compoments.Add(new Beam(height));
-                j++;
-            }
-        }
+			int j = 0;
+			while (j < 2) {
+				Compoments.Add(new Beam(height));
+				j++;
+			}
+		}
 
-        private void AddDoorBeam(int height)
-        {
-            int j = 0;
-            while (j < 2)
-            {
-                Compoments.Add(new DoorBeam(height));
-                j++;
-            }
-        }
-    }
+		private void AddDoorBeam(int height)
+		{
+			int j = 0;
+			while (j < 2) {
+				Compoments.Add(new DoorBeam(height));
+				j++;
+			}
+		}
+	}
 }
