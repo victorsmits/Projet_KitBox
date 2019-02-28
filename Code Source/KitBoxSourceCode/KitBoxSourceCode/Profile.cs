98727813c480d65550b0e5695d015e2a5f6a9115
victorsmits <victor.smits@student.vinci.be>
2019-02-28 13:46:58 +0100

    Profile code start

--- Code Source/KitBoxSourceCode/KitBoxSourceCode/Profile.cs
+++ Code Source/KitBoxSourceCode/KitBoxSourceCode/Profile.cs
@@ -3,8 +3,30 @@ namespace KitBoxSourceCode
 {
     public class Profile
     {
-        public Profile()
+        private string FirstName;
+        private string LastName;
+        private string Email;
+
+        public Profile(string LN, string FN, string mail = null)
         {
+            FirstName = FN;
+            LastName = LN;
+            Email = mail;
+        }
+
+        public string GetEmail()
+        {
+            return GetEmail();
+        }
+
+        public string GetLastName()
+        {
+            return GetLastName();
+        }
+
+        public string GetFirstName()
+        {
+            return GetFirstName();
         }
     }
 }
