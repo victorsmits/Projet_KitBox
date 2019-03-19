ce8714e69821fbe04f4e04ad4d54e67f1498f86b
Michaël <micscarito@gmail.Com>
2019-03-19 22:23:10 +0100

    working on the generation of the cart (visualy)

--- Interface/KitBoxApplication/KitBoxApplication/CartPage.cs
+++ Interface/KitBoxApplication/KitBoxApplication/CartPage.cs
@@ -36,11 +36,47 @@ namespace KitBoxApplication
         private void Create_Layout()
         {
             MessageBox.Show(jsonCart.ToString());
-            JToken totalCart = jsonCart.First;
-            JToken firstCabinet = totalCart.First;
-            MessageBox.Show(totalCart.ToString());
+
+            JToken firstCabinet = jsonCart.First;
+            JToken firstFloor = firstCabinet.First;
+            JToken nextCabinet = firstCabinet.Next;
+            JToken nextFloor = firstFloor.Next;
+
+            MessageBox.Show(jsonCart.Count.ToString());
             MessageBox.Show(firstCabinet.ToString());
-            MessageBox.Show(jsonCart.First.First.First.Next.ToString());
+            //Generates the first cabinet tab
+            TabPage cabine1Page = new TabPage(Name = "Cabine 1");
+            cabine1Page.BackColor = Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(51)))));
+            Label floorCab1 = new Label();
+            floorCab1.Text = "floor : ";
+            floorCab1.Location = new Point(29, 26);
+            cabine1Page.Controls.Add(floorCab1);
+            tabControl1.TabPages.Clear();
+            tabControl1.TabPages.Add(cabine1Page);
+
+            try
+            {
+                for (int cabinetNumber = 0; cabinetNumber <= jsonCart.Count; cabinetNumber++)
+                {
+                
+                    MessageBox.Show("test longueur : " + (cabinetNumber).ToString());
+                    MessageBox.Show(nextCabinet.ToString());
+                    //Generates the following cabinet tabs
+                    TabPage addedCabinePage = new TabPage(Name = "Cabine "+ (cabinetNumber + 2).ToString());
+                    addedCabinePage.BackColor = Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(51)))));
+                    Label floor = new Label();
+                    floor.Text = "floor : ";
+                    floor.Location = new Point(29, 26);
+                    addedCabinePage.Controls.Add(floor);
+                    tabControl1.TabPages.Add(addedCabinePage);
+                    nextCabinet = nextCabinet.Next; 
+                }
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show("c'est tout : ");
+            }
+
         }
 
 
